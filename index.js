bbeb67c799de02eead43ca730b0d405f249b3dbd
zuedev <zuedev@gmail.com>
2020-04-16 11:42:28 +0100

    add cookbook

--- index.js
+++ index.js
@@ -2,5 +2,18 @@ $(document).ready(() => {
   document.querySelectorAll("pre code").forEach((block) => {
     hljs.highlightBlock(block);
   });
+
+  window.zState = {
+    page: "#home",
+  };
+
   $("body").fadeIn();
 });
+
+function goto(x) {
+  $(window.zState.page).fadeOut(400, () => {
+    $(x).fadeIn(400, () => {
+      window.zState.page = x;
+    });
+  });
+}
